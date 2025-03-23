638ab1eb65cecfb5555a3c3f45747a878eb62f9a
bajacqueli <bastien.jacquelin@etu.uca.fr>
2025-03-23 15:12:10 +0100

    Add sensors table to database initialization script

--- Domotic/Data/init.sql
+++ Domotic/Data/init.sql
@@ -1,3 +1,9 @@
+CREATE TABLE sensors (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    location VARCHAR(255)
+);
+
 CREATE TABLE measurements (
     id SERIAL PRIMARY KEY,
     timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -5,10 +11,4 @@ CREATE TABLE measurements (
     humidity FLOAT,
     light FLOAT,
     idSensor INT REFERENCES sensors(id)
-);
-
-CREATE TABLE sensors (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255),
-    location VARCHAR(255)
 );
