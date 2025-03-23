378e1830058e10a14f8533a973e15a78a4740b46
bajacqueli <bastien.jacquelin@etu.uca.fr>
2025-03-23 15:36:55 +0100

    broker connection after api call

--- Domotic/api/rgb.py
+++ Domotic/api/rgb.py
@@ -14,7 +14,6 @@ MQTT_PORT = int(os.getenv('MQTT_PORT', 1883))
 app = FastAPI()
 client = mqtt.Client(client_id="api")
 client.username_pw_set(MQTT_USER, MQTT_PASSWORD)
-client.connect(MQTT_BROKER, MQTT_PORT, 60)
 
 @app.get("/setColor")
 def set_color(
@@ -23,6 +22,7 @@ def set_color(
     b: int = Query(..., description="Blue value"),
     brightness: int = Query(..., description="Brightness value")
 ):
+    client.connect(MQTT_BROKER, MQTT_PORT, 60)
     message = f"{r},{g},{b},{brightness}"
     client.publish(MQTT_TOPIC, message)  # Change mqtt_client to client
     connection_status = "Connected" if client.is_connected() else "Not connected"
